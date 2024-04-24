9f9339a6032831feedbae936b9093755be73ec87
sarzuricarlos <sarzuricarlos60@gmail.com>
2024-04-23 23:18:06 -0400

    Inicio Cita

--- assets/java.js
+++ assets/java.js
@@ -7,21 +7,20 @@ num2 = 15;
 num3 = 20;
 num4 = 15;
 cont = 0;
-tam = 2.5;
+tam = 100;
 
 cajaNo.addEventListener('click',()=>
 {
     cont++;
-    num1 = num1+0.2;
-    num2 = num2+0.5;
+    num1 = num1+0.5;
+    num2 = num2+0.2;
     num3 = num3-0.2;
     num4 = num4-0.1;
-    tam = tam+0.1;
+    tam = tam+10;
     cajaCita.style.position = "relative";
-    cajaSi.style.zIndex = cont;
     cajaSi.style.width = num1+"vw";
     cajaSi.style.height = num2+"vh";
-    cajaSi.style.fontSize = tam+"em";
+    cajaSi.style.fontSize = tam+"%";
     cajaNo.style.width = num3+"vw";
     cajaNo.style.height = num4+"vh";
     if(cont==10){
